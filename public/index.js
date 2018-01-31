167542c515acd30cd24dc283e427e08700373c64
Phil Mitchell <pm1873@gmail.com>
2018-01-31 13:25:27 +0000

    select box working

--- public/index.js
+++ public/index.js
@@ -5,12 +5,21 @@ var makeRequest = function(url, callback) {
   request.send();
 };
 
-var populateList = function(countries) {
-  var ul = document.querySelector('#country-list');
+// var populateList = function(countries) {
+//   var ul = document.querySelector('#country-list');
+//   countries.forEach(function(country) {
+//     var li = document.createElement('li');
+//     li.innerText = country.name;
+//     ul.appendChild(li);
+//   })
+// }
+
+var populateSelectBox = function(countries) {
+  var selectBox = document.querySelector('#countries-dropdown');
   countries.forEach(function(country) {
-    var li = document.createElement('li');
-    li.innerText = country.name;
-    ul.appendChild(li);
+    var option = document.createElement('option');
+    option.innerText = country.name;
+    selectBox.appendChild(option);
   })
 }
 
@@ -21,19 +30,18 @@ var requestComplete = function() {
   }
   var jsonString = this.responseText;
   var countries = JSON.parse(jsonString);
-  populateList(countries);
+  // populateList(countries);
+  populateSelectBox(countries);
 };
 
-var url = 'https://restcountries.eu/rest/v2';
 
-var handleButtonClick = function() {
-  makeRequest(url, requestComplete);
-}
+// var handleButtonClick = function() {
+//   makeRequest(url, requestComplete);
+// }
 
 var app = function() {
-  // makeRequest(url, requestComplete);
-  var button = document.querySelector('#populate-countries')
-  button.addEventListener('click', handleButtonClick);
+  var url = 'https://restcountries.eu/rest/v2';
+  makeRequest(url, requestComplete);
 };
 
 window.addEventListener('load', app);
